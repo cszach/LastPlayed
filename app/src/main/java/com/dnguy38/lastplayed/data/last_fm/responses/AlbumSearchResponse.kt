4bb80f29d64b3bc81ed901b346d0d16940c90d91
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-04-24 05:31:40 -0400

    Reorder methods in AlbumSearchResponse.kt

--- app/src/main/java/com/dnguy38/lastplayed/data/last_fm/responses/AlbumSearchResponse.kt
+++ app/src/main/java/com/dnguy38/lastplayed/data/last_fm/responses/AlbumSearchResponse.kt
@@ -5,6 +5,18 @@ import com.dnguy38.lastplayed.data.search.SearchResult
 import com.dnguy38.lastplayed.data.search.SearchType
 import com.google.gson.annotations.SerializedName
 
+data class AlbumSearchResponse(
+    val results: AlbumSearchResults
+)
+
+data class AlbumSearchResults(
+    @SerializedName("albummatches") val matches: AlbumMatches
+)
+
+data class AlbumMatches(
+    val album: List<AlbumMatch>
+)
+
 data class AlbumMatch(
     override val name: String,
     val artist: String,
@@ -16,15 +28,3 @@ data class AlbumMatch(
     override val type: SearchType
         get() = SearchType.Album
 }
-
-data class AlbumMatches(
-    val album: List<AlbumMatch>
-)
-
-data class AlbumSearchResults(
-    @SerializedName("albummatches") val matches: AlbumMatches
-)
-
-data class AlbumSearchResponse(
-    val results: AlbumSearchResults
-)
