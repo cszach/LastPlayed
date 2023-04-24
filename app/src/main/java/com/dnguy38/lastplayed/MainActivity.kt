0c5f3df898e256ca770a537a95582bee0184f252
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-04-24 08:11:59 -0400

    Application menu

--- app/src/main/java/com/dnguy38/lastplayed/MainActivity.kt
+++ app/src/main/java/com/dnguy38/lastplayed/MainActivity.kt
@@ -2,10 +2,14 @@ package com.dnguy38.lastplayed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.dnguy38.lastplayed.ui.info.InfoFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -35,5 +39,28 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settingsFragment -> true
+            R.id.infoFragment -> {
+                val infoFragment = InfoFragment.newInstance()
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.nav_host_fragment, infoFragment)
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
